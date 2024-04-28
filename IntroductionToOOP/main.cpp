eb7d25a4319b0b0db31597df81a447ff6a2cf453
Lapin_qh81@top-academy.ru <Lapin_qh81@top-academy.ru>
2024-04-28 11:54:40 +0900

    Add project files.

---
+++ IntroductionToOOP/main.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Point	//Cтруктура которая будет описывать точки на плоскости.
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+
+//#define STRUCT_POINT
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
+	int a; // Объявление переменной а типа инт 
+	Point A; //Объявление переменной А типа Поинт
+	//Создание объекта A структуры Point 
+	//Создание экземпляра структуры Point 
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << (*pA).x << "\t" << (*pA).y << endl;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+}
