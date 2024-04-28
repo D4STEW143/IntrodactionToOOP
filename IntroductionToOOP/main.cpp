babe46f31f4296381d89a49353338c6ff7351b2d
Lapin_qh81@top-academy.ru <Lapin_qh81@top-academy.ru>
2024-04-28 19:07:58 +0900

    Homework done.

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -23,7 +23,18 @@ public:
 	{
 		this->y = y;
 	}
+	//Methods
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
 };
+
+double Distance(Point A, Point B);
+
 
 //#define STRUCT_POINT
 
@@ -43,9 +54,35 @@ void main()
 	cout << (*pA).x << "\t" << (*pA).y << endl;
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POINT
+	int cor_xA, cor_yA, cor_xB, cor_yB;
+	cout << "Укажите координаты точки А. Координата Х: ";
+	cin >> cor_xA;
+	cout << "Координата Y: ";
+	cin >> cor_yA;
+	cout << "Укажите координаты точки B. Координата Х: ";
+	cin >> cor_xB;
+	cout << "Координата Y: ";
+	cin >> cor_yB;
 
-	Point A;
-	A.set_x(2);
-	A.set_y(3);
+	Point A, B;
+	A.set_x(cor_xA);
+	A.set_y(cor_yA);
+	B.set_x(cor_xB);
+	B.set_y(cor_yB);
+
 	cout << A.get_x() << "\t" << A.get_y() << endl;
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+
+	cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << "Расстояние от точки А до точки В: " << B.distance(A) << endl;
+	cout << "Расстояние между точками А и В: " << Distance(A, B) << endl;
+	cout << "Расстояние между точками В и А: " << Distance(A, B) << endl;
+}
+
+double Distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
 }
